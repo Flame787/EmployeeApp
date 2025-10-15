652af0257208523f6351f280555927d8fffb35bc
Flame787 <mbrezovic77@gmail.com>
2025-10-15 20:31:20 +0200

    corrected server errors

--- node-server/db.js
+++ node-server/db.js
@@ -1,17 +1,19 @@
-const sql = require('mssql')
+// const sql = require('mssql')
+const mysql = require("mysql2/promise");
 require("dotenv").config();
+console.log("DB_USER:", process.env.DB_USER);
 
 // configuration for sql-server:
 
 const config = {
-  server: process.env.DB_SERVER,
+  host: process.env.DB_SERVER,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD, 
-  options: {
-    encrypt: false,    // or true if using Azure sql
-	enableArithAbort: true
-  },
+//   options: {
+//     encrypt: false,    // or true if using Azure sql
+// 	enableArithAbort: true
+//   },
   port: parseInt(process.env.DB_PORT)
 }
 
@@ -19,19 +21,20 @@ const config = {
 
 // create a connection pool to database and export it as a promise:
 
-const poolPromise = new sql.ConnectionPool(config)
-  .connect()
-  .then(pool => {
-    console.log("Connected to database");
-    return pool;
-  })
-  .catch(err => {
-    console.error("Database connection failed", err);
-    throw err;
-  });
+// const poolPromise = new sql.ConnectionPool(config)
+const poolPromise = mysql.createPool(config)
+//   .connect()
+//   .then(pool => {
+//     console.log("Connected to database");
+//     return pool;
+//   })
+//   .catch(err => {
+//     console.error("Database connection failed", err);
+//     throw err;
+//   });
 
 module.exports = {
-  sql,
+  sql: mysql,
   poolPromise
 };
 
--- node-server/server.js
+++ node-server/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () =>  console.log(`Server is running on port ${PORT}`));
 
 
-// for test in terminalu: node servers.js - when started, should write 'Server is running on port...'
+// for test in terminalu: node server.js - when started, should write 'Server is running on port...'
 
 // creating 1st API: get all employee records
 
@@ -19,12 +19,12 @@ app.get("/api/employees", async(req, res) => {
    try{
      // create pull object:
      const pool = await poolPromise;
-	 const result = await pool.request().query(
+	 const [rows] = await pool.query(
         "SELECT * FROM DummyEmployees");
-	 console.log(result);
+	 console.log(rows);
 	 res.status(200).json({
 	    success: true,
-		empData: result.recordset     // empData = employee data
+		empData: rows     // empData = employee data
 	 })
    }
    
